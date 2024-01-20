aa6a9adc6ea53b511c4e7f8ebbbb23e7c5412f70
chatbca <137300097+chatbca@users.noreply.github.com>
2024-01-20 18:32:33 +0530

    A3,Prime.R

--- A3,Prime.R
+++ A3,Prime.R
@@ -1,41 +1,23 @@
-is_prime<-function(n)
-{
-  if(n<=1)
-  {
-    return(FALSE)
-  }else if(n<=3)
-  {
-    return(TRUE)
-  }else if(n%%2==0||n%%3==0)
-  {
-    return(FALSE)
-  }
-  i<-5
-  while(i*i<=n)
-  {
-    if(n %%i==0||n %%(i+2)==0)
-    {
-      return(FALSE)
-    }
-    i<-i+6
-  }
-  return(TRUE)
-}
-find_primes_numberws<-function(start,end)
-{
-  primes<-c()
-  for(i in start:end)
-  {
-    if(is_prime(i))
-    {
-      primes<-c(primes,i)
-    }
-  }
-  return(c(primes))
-}
-cat("\n... To find prime numbers between interval...")
-st=as.integer(readline("Enter the start number of interval:"))
-ed=as.integer(readline("Enter the end number of interval:"))
-result<-find_primes_numberws(st,ed)
-cat("Prime numbers between ",st,"and",ed,"are:")
-print(result)
+cat("\014")
+generate_primes <- function(start, end) { 
+  prime_numbers <- vector("numeric", 0) 
+  for (i in start:end) { 
+    is_prime <- TRUE 
+    for (j in 2:sqrt(i)) { 
+      if (i %% j == 0) { 
+        is_prime <- FALSE 
+        break 
+      } 
+    } 
+    if (is_prime) { 
+      prime_numbers <- c(prime_numbers, i) 
+    } 
+  } 
+  return(prime_numbers) 
+} 
+start <- as.numeric(readline("Enter the starting number: ")) 
+end <- as.numeric(readline("Enter the ending number: ")) 
+prime_numbers <- generate_primes(start, end) 
+cat("Prime numbers between", start, "and", end, "are:\n",prime_numbers) 
+cat("The prime numbers between", start, "and", end, "are:", 
+            prime_numbers)
